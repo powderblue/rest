6e6cc035e9da2664eabeb38c0346091dfa33ebb5
Daniel Bettles <dan@powder-blue.com>
2024-03-18 18:03:17 +0000

    Did a little clean-up while I was looking for something

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Doctrine 2 REST Server and Client
+# Fork of Doctrine 2 REST Server and Client
+
+> [!WARNING]
+> This fork of [doctrine/rest](https://github.com/doctrine/rest) is heavily customised and probably unsuitable for any other project
--- lib/Doctrine/REST/Client/Client.php
+++ lib/Doctrine/REST/Client/Client.php
@@ -22,8 +22,10 @@
 
 namespace Doctrine\REST\Client;
 
+use CurlHandle;
 use Doctrine\REST\Exception\HttpException;
 use Exception;
+use RuntimeException;
 
 use const CURLOPT_USERAGENT;
 use const false;
@@ -47,28 +49,36 @@ class Client
     public function post(Request $request)
     {
         $request->setMethod(Client::POST);
+
         return $this->execute($request);
     }
 
     public function get(Request $request)
     {
         $request->setMethod(Client::GET);
+
         return $this->execute($request);
     }
 
     public function put(Request $request)
     {
         $request->setMethod(Client::PUT);
+
         return $this->execute($request);
     }
 
     public function delete(Request $request)
     {
         $request->setMethod(Client::DELETE);
+
         return $this->execute($request);
     }
 
-    private function createCurl(Request $request)
+    /**
+     * @throws RuntimeException If it failed to initialise a cURL session
+     * @throws RuntimeException If it failed to set a cURL option
+     */
+    private function createCurl(Request $request): CurlHandle
     {
         $options = array(
             CURLOPT_URL => $request->getUrl(),
@@ -92,45 +102,60 @@ class Client
             case self::PUT:
                 $options[CURLOPT_POST] = 1;
                 $options[CURLOPT_POSTFIELDS] = http_build_query($request->getParameters());
+
                 break;
 
             case self::DELETE:
                 $options[CURLOPT_CUSTOMREQUEST] = 'DELETE';
+
                 break;
         }
 
         $curl = curl_init();
-        curl_setopt_array($curl, $options);
+
+        if (false === $curl) {
+            throw new RuntimeException('Failed to initialise a cURL session');
+        }
+
+        if (false === curl_setopt_array($curl, $options)) {
+            throw new RuntimeException('Failed to set a cURL option');
+        }
 
         return $curl;
     }
 
+    /**
+     * @throws Exception If it failed to perform a cURL session
+     * @throws HttpException If the HTTP request was unsuccessful
+     */
     public function execute(Request $request)
     {
         $curl = $this->createCurl($request);
-        $result = curl_exec($curl);
 
-        if ($result === false) {
-            $errorMessage = curl_errno($curl) . ': ' . curl_error($curl);
+        try {
+            $result = curl_exec($curl);
+
+            if ($result === false) {
+                $errorMessage = curl_errno($curl) . ': ' . curl_error($curl);
+
+                throw new Exception(implode(' | ', [
+                    $errorMessage,
+                    $request->getUrl(),
+                    http_build_query($request->getParameters()),
+                    $request->getUsername(),
+                ]));
+            }
+
+            /** @var int */
+            $httpStatusCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+            if ($httpStatusCode >= 400 && $httpStatusCode < 600) {
+                throw new HttpException('The HTTP request was unsuccessful', $httpStatusCode);
+            }
+
+            return $request->getResponseTransformerImpl()->transform($result);
+        } finally {
             curl_close($curl);
-
-            throw new Exception(implode(' | ', [
-                $errorMessage,
-                $request->getUrl(),
-                http_build_query($request->getParameters()),
-                $request->getUsername(),
-            ]));
         }
-
-        $httpStatusCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-
-        if (in_array(substr($httpStatusCode, 0, 1), array(4, 5))) {
-            curl_close($curl);
-            throw new HttpException('The HTTP request was unsuccessful', $httpStatusCode);
-        }
-
-        curl_close($curl);
-
-        return $request->getResponseTransformerImpl()->transform($result);
     }
 }
